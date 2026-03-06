3f88d577653cd19281fd9385a7d8ef8d181c95f3
Riley Hendrickson <rileyhe1@gmail.com>
2026-03-06 14:34:00 -0800

    new practice test file, may be removed later

--- src/test/java/rileyhe1/jobapplicationtracker/repositories/CompanyRepositoryTests.java
+++ src/test/java/rileyhe1/jobapplicationtracker/repositories/CompanyRepositoryTests.java
@@ -1,4 +1,26 @@
 package rileyhe1.jobapplicationtracker.repositories;
 
-public class CompanyRepositoryTests {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import rileyhe1.jobapplicationtracker.entities.Company;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class CompanyRepositoryTests
+{
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Test
+    public void companyRepository_ReturnSavedCompanies()
+    {
+        // arrange
+
+        // act
+
+        // assert
+    }
 }
